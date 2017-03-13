e516eeacf74ebfdda94c5dfe2000218fbf325098
Manoel Vilela <manoel_vilela@engineer.com>
2017-03-13 12:56:08 -0300

    Simplify Python client version: remove ncurses stuff
    Because ncurses is a curse. And that way is better to compare with the
    Lisp version.
    
    Sometimes I got race conditions problem with server_reader routine
    and read_stream.close() on finally cause.

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # Author: Manoel Vilela
 
-import curses
 import socket
 import threading
 
@@ -14,148 +13,36 @@ read_stream = s.makefile()
 write_stream = s.makefile(mode='w')
 
 
-class ChatUI(object):
-    def __init__(self, stdscr):
-        curses.use_default_colors()
-        for i in range(0, curses.COLORS):
-            curses.init_pair(i, i, -1)
-        self.stdscr = stdscr
-        self.inputbuffer = ""
-        self.linebuffer = []
-        self.chatbuffer = []
-
-        # Curses, why must you confuse me with your height, width, y, x
-        chatbuffer_hwyx = (curses.LINES - 2, curses.COLS-1, 0, 1)
-        chatline_yx = (curses.LINES - 1, 0)
-        self.win_chatline = stdscr.derwin(*chatline_yx)
-        self.win_chatbuffer = stdscr.derwin(*chatbuffer_hwyx)
-        self.redraw_ui()
-
-    def resize(self):
-        """Handles a change in terminal size"""
-        h, w = self.stdscr.getmaxyx()
-
-        self.win_chatline.mvwin(h - 1, 0)
-        self.win_chatline.resize(1, w)
-        self.win_chatbuffer.resize(h - 2, w - 2)
-
-        self.linebuffer = []
-        for msg in self.chatbuffer:
-            self._linebuffer_add(msg)
-
-        self.redraw_ui()
-
-    def redraw_ui(self):
-        """Redraws the entire UI"""
-        h, w = self.stdscr.getmaxyx()
-        self.stdscr.clear()
-        self.stdscr.vline(0, 1, "|", h - 2)
-        self.stdscr.hline(h - 2, 0, "-", w)
-        self.stdscr.refresh()
-
-        self.redraw_chatbuffer()
-        self.redraw_chatline()
-
-    def redraw_chatline(self):
-        """Redraw the user input textbox"""
-        h, w = self.win_chatline.getmaxyx()
-        self.win_chatline.clear()
-        start = len(self.inputbuffer) - w + 1
-        if start < 0:
-            start = 0
-        self.win_chatline.addstr(0, 0, self.inputbuffer[start:])
-        self.win_chatline.refresh()
-
-    def redraw_chatbuffer(self):
-        """Redraw the chat message buffer"""
-        self.win_chatbuffer.clear()
-        h, w = self.win_chatbuffer.getmaxyx()
-        j = len(self.linebuffer) - h
-        if j < 0:
-            j = 0
-        for i in range(min(h, len(self.linebuffer))):
-            self.win_chatbuffer.addstr(i, 0, self.linebuffer[j])
-            j += 1
-        self.win_chatbuffer.refresh()
-
-    def chatbuffer_add(self, msg):
-        """Add a message to the chat buffer, automatically slicing it to
-        fit the width of the buffer
-        """
-        self.chatbuffer.append(msg)
-        self._linebuffer_add(msg)
-        self.redraw_chatbuffer()
-        self.redraw_chatline()
-        self.win_chatline.cursyncup()
-
-    def _linebuffer_add(self, msg):
-        h, w = self.stdscr.getmaxyx()
-        w = w - 2
-        while len(msg) >= w:
-            self.linebuffer.append(msg[:w])
-            msg = msg[w:]
-        if msg:
-            self.linebuffer.append(msg)
-
-    def prompt(self, msg):
-        """Prompts the user for input and returns it"""
-        self.inputbuffer = msg
-        self.redraw_chatline()
-        res = self.wait_input()
-        res = res[len(msg):]
-        return res
-
-    def wait_input(self, prompt=""):
-        """Wait for the user to input a message and hit enter.
-        Returns the message
-        """
-        self.inputbuffer = prompt
-        self.redraw_chatline()
-        self.win_chatline.cursyncup()
-        last = -1
-        while last != ord('\n'):
-            last = self.stdscr.getch()
-            if last == ord('\n'):
-                tmp = self.inputbuffer
-                self.inputbuffer = ""
-                self.redraw_chatline()
-                self.win_chatline.cursyncup()
-                return tmp[len(prompt):]
-            elif last == curses.KEY_BACKSPACE or last == 127:
-                if len(self.inputbuffer) > len(prompt):
-                    self.inputbuffer = self.inputbuffer[:-1]
-            elif last == curses.KEY_RESIZE:
-                self.resize()
-            elif 32 <= last <= 126:
-                self.inputbuffer += chr(last)
-            self.redraw_chatline()
+def get_user_input():
+    inp = input()
+    print("\033[1A\033[2K", end='')
+    return inp
 
 
-def writer():
-    """Fetch lines from server and fill the chatbuffer"""
-    global chat
+def server_reader():
+    """Fetch lines from server and print"""
     while not read_stream.closed:
-        chat.chatbuffer_add(read_stream.readline())
+        print(read_stream.readline(), end='')
 
 
-def main(stdscr):
-    """Main routine to be wrapped on curses.wrapper"""
-    global chat
-    stdscr.clear()
-    chat = ChatUI(stdscr)
+def send_message(message):
+    write_stream.write(message + '\n')
+    write_stream.flush()
+
+
+def main():
     try:
-        username = chat.wait_input(str(read_stream.readline().strip('\n')))
-        write_stream.write(username + '\n')
-        write_stream.flush()
+        username = input(str(read_stream.readline().strip('\n')))
+        print("Connected as {}@{}:{}".format(username, HOST, PORT))
+        send_message(username)
         inp = ""
-        t = threading.Thread(target=writer)
+        t = threading.Thread(target=server_reader)
         t.start()
         while inp != "/quit":
-            inp = chat.wait_input()
-            write_stream.write(inp + '\n')
-            write_stream.flush()
+            inp = get_user_input()
+            send_message(inp)
     except KeyboardInterrupt:
-        pass
+        send_message("/quit")
     finally:
         read_stream.close()
         write_stream.close()
@@ -163,4 +50,5 @@ def main(stdscr):
         t.join()
 
 
-curses.wrapper(main)
+if __name__ == '__main__':
+    main()
