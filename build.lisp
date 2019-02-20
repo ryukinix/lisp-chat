f515e19745fe5c28d807311f91a3e9fc5519bc00
Manoel Vilela <manoel_vilela@engineer.com>
2019-02-20 13:51:49 -0300

    Trying support dynamic library distribution
    It should working by looking for currently libraries available.
    But LD_LIBRARY_PATH should works here. I'm thinking there is some
    problem about cffi:*foreign-library-directories* part.

--- build.lisp
+++ build.lisp
@@ -1,7 +1,23 @@
-(pushnew (truename (sb-unix:posix-getcwd/)) ql:*local-project-directories* )
+(pushnew (truename (sb-unix:posix-getcwd/)) ql:*local-project-directories*)
 (ql:register-local-projects)
-(ql:quickload :lisp-chat)
+(ql:quickload '(:lisp-chat :cffi))
+
+(defun main()
+  (let ((libpath (sb-posix:getcwd)))
+    (format t "=> New LD_LIBRARY_PATH: ~a~%" libpath)
+    (pushnew libpath
+             cffi:*foreign-library-directories*
+             :test #'equal)
+    (cffi:define-foreign-library libreadline
+      (t "libreadline.so"))
+    (cffi:use-foreign-library libreadline))
+  (lisp-chat-client:main))
+
+;; close currently foreign libraries loaded
+(loop for library in (cffi:list-foreign-libraries :loaded-only t)
+      do (cffi:close-foreign-library library))
+
 (sb-ext:save-lisp-and-die "lisp-chat"
-                          :toplevel #'lisp-chat-client:main
+                          :toplevel #'main
                           :executable t
-                          :compression 9)
+                          :compression 1)
