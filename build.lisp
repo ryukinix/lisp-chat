025fd3a48a1d24003de46da76cc546baa2987051
Manoel Vilela <manoel_vilela@engineer.com>
2019-02-20 14:17:58 -0300

    Fix build.lisp dynamic load by using uiop/os:getcwd
    There is a semantic difference of a pathname and a string in Common
    Lisp. sb-posix:getcwd only return a string and it doesn't works really
    well with cffi:*foreign-library-directories*.
    
    This fix everything! Now I can deploy the software with the dynamic
    libraries in the root of the directory of lisp-chat binary.

--- build.lisp
+++ build.lisp
@@ -2,15 +2,27 @@
 (ql:register-local-projects)
 (ql:quickload '(:lisp-chat :cffi))
 
-(defun main()
-  (let ((libpath (sb-posix:getcwd)))
-    (format t "=> New LD_LIBRARY_PATH: ~a~%" libpath)
+(eval-when (:compile-toplevel :execute)
+    (defconstant +debug+ nil))
+
+(defmacro debug-format (&rest body)
+  (when +debug+
+    `(format t ,@body)))
+
+(defun import-foreign-libraries ()
+  (let ((libpath (uiop/os:getcwd)))
+    (debug-format "=> New LD_LIBRARY_PATH: ~a~%" libpath)
     (pushnew libpath
              cffi:*foreign-library-directories*
              :test #'equal)
     (cffi:define-foreign-library libreadline
       (t "libreadline.so"))
     (cffi:use-foreign-library libreadline))
+  (debug-format "Loaded libraries: ~a~%" (cffi:list-foreign-libraries)))
+
+
+(defun main()
+  (import-foreign-libraries)
   (lisp-chat-client:main))
 
 ;; close currently foreign libraries loaded
