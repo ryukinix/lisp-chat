7ac9b9367ba5f0e8913a39d5098ae97efa7af037
Manoel Vilela <manoel_vilela@engineer.com>
2020-11-06 22:06:35 -0300

    Set lisp-chat/config:*host* to localhost

--- src/config.lisp
+++ src/config.lisp
@@ -10,5 +10,5 @@
 (in-package :lisp-chat/config)
 
 (defparameter *debug* t "Run application in debug mode with extra info in terminal")
-(defparameter *host* "server.lerax.me" "Host used in server and client")
+(defparameter *host* "localhost" "Host used in server and client")
 (defparameter *port* 5558 "Default port")
