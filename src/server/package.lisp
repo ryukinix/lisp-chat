087508945f5b41a2210ae6d320736216ca7e6a65
Manoel Vilela <manoel.neto@neoway.com.br>
2026-01-28 06:55:49 -0300

    refactor: split single file server into modules

---
+++ src/server/package.lisp
@@ -0,0 +1,19 @@
+;; Common Lisp Script
+;; Manoel Vilela
+
+(defpackage #:lisp-chat/server
+  (:use #:usocket
+        #:cl
+        #:lisp-chat/config
+        #:bordeaux-threads)
+  (:import-from #:websocket-driver
+                #:make-server
+                #:ws
+                #:on
+                #:send
+                #:ready-state
+                #:start-connection)
+  (:import-from #:clack
+                #:clackup
+                #:stop)
+  (:export #:main))
---
+++ src/server/tcp.lisp
@@ -0,0 +1,71 @@
+(in-package :lisp-chat/server)
+
+(defun client-reader-routine (client)
+  "This function create a IO-bound procedure to act
+   by reading the events of a specific CLIENT.
+   On this software each client talks on your own thread."
+  (loop for message := (read-line (client-stream client))
+        while (not (equal message "/quit"))
+        for response := (call-command client message)
+        if response
+          do (send-message client response)
+        else
+          when (> (length message) 0)
+            do (push-message (client-name client)
+                             message)
+        finally (client-delete client)))
+
+(defun client-reader (client)
+  "This procedure is a wrapper for CLIENT-READER-ROUTINE
+   treating all the possible errors based on HANDLER-CASE macro."
+  (handler-case (client-reader-routine client)
+    (end-of-file () (client-delete client))
+    (#+sbcl sb-int:simple-stream-error
+     #-sbcl error
+     ()
+      (progn (debug-format t "~a@~a timed output"
+                           (client-name client)
+                           (client-address client))
+             (client-delete client)))
+    (#+sbcl sb-bsd-sockets:not-connected-error
+     #-sbcl error
+     ()
+      (progn (debug-format t "~a@~a not connected more."
+                           (client-name client)
+                           (client-address client))
+             (client-delete client)))))
+
+(defun create-client (connection)
+  "This procedure create a new client based on CONNECTION made by
+  USOCKET:SOCKET-ACCEPT. This shit create a lot of side effects as messages
+  if the debug is on because this makes all the log stuff to make analysis"
+  (debug-format t "Incoming connection from ~a ~%" (socket-peer-address connection))
+  (let ((client-stream (socket-stream connection)))
+    (write-line "> Type your username: " client-stream)
+    (finish-output client-stream)
+    (let ((client (make-client :name (read-line client-stream)
+                               :socket connection
+                               :address (socket-peer-address connection))))
+      (with-lock-held (*client-lock*)
+        (debug-format t "Added new user ~a@~a ~%"
+                      (client-name client)
+                      (client-address client))
+        (push client *clients*))
+      (push-message "@server" (format nil "The user ~s joined to the party!" (client-name client)))
+      (make-thread (lambda () (client-reader client))
+                   :name (format nil "~a reader thread" (client-name client))))))
+
+;; a function defined to handle the errors of client thread
+(defun safe-client-thread (connection)
+  "This function is a wrapper for CREATE-CLIENT treating the
+exceptions."
+  (handler-case (create-client connection)
+    (end-of-file () nil)
+    (usocket:address-in-use-error () nil)))
+
+(defun connection-handler (socket-server)
+  "This is a special thread just for accepting connections from SOCKET-SERVER
+   and creating new clients from it."
+  (loop for connection = (socket-accept socket-server)
+        do (make-thread (lambda () (safe-client-thread connection))
+                        :name "create client")))
