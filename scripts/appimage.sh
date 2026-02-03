bc24f5c3e219f3a55df1717ce081739bf446218c
Manoel V. Machado <manoel_vilela@engineer.com>
2026-02-03 14:56:38 -0300

    feat: add appimage script (#36)
    * feat: add appimage script
    
    * chore: update gitignore
    
    * feat: embed gnu readline dynamic lib into appimage

---
+++ scripts/appimage.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+APPIMAGEDIR=.appimage
+mkdir -p "$APPIMAGEDIR"
+
+APPIMAGE_TOOL_URL="https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+APPIMAGE_TOOL_PATH="$APPIMAGEDIR/appimagetool-x86_64.AppImage"
+
+if [ ! -f "$APPIMAGE_TOOL_PATH" ]; then
+    echo "Downloading appimagetool..."
+    wget -c "$APPIMAGE_TOOL_URL" -O "$APPIMAGE_TOOL_PATH"
+    chmod +x "$APPIMAGE_TOOL_PATH"
+else
+    echo "appimagetool already exists, skipping download."
+fi
+
+APPDIR="$APPIMAGEDIR/lisp-chat.AppDir"
+
+# Create the AppDir structure
+mkdir -p "$APPDIR/usr/bin/"
+
+# Create the .desktop file
+cat > "$APPDIR/lisp-chat.desktop" <<EOL
+[Desktop Entry]
+Name=lisp-chat
+Exec=AppRun
+Icon=computer-symbolic
+Terminal=true
+Type=Application
+Categories=Utility;
+EOL
+
+# Create AppRun
+cat > "$APPDIR/AppRun" <<EOL
+#!/bin/sh
+HERE=\$(dirname "\$(readlink -f "\${0}")")
+"\${HERE}/usr/bin/lisp-chat" "\$@"
+EOL
+chmod +x "$APPDIR/AppRun"
+
+# Copy the binary and icon
+cp -v ./roswell/lisp-chat "$APPDIR/usr/bin/"
+cp -v /usr/share/icons/Adwaita/scalable/devices/computer-symbolic.svg "$APPDIR/"
+
+# Copy libreadline and symlink
+mkdir -p "$APPDIR/usr/lib/"
+READLINE_SYMLINK=$(ldconfig -p | grep libreadline | cut -d '>' -f 2 | tr -d ' ' | grep "libreadline.so.[0-9]" | head -n 1)
+if [ -n "$READLINE_SYMLINK" ]; then
+    READLINE_REAL=$(readlink -f "$READLINE_SYMLINK")
+    cp -v "$READLINE_REAL" "$APPDIR/usr/lib/"
+    cp -P -v "$READLINE_SYMLINK" "$APPDIR/usr/lib/"
+else
+    echo "Warning: libreadline not found via ldconfig"
+fi
+
+# Run appimagetool
+"$APPIMAGE_TOOL_PATH" "$APPDIR"
