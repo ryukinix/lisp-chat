8028e455d38c53798f63158fd32bdb60517102c9
Manoel Vilela <manoel_vilela@engineer.com>
2026-07-21 13:02:03 -0300

    revert: restore index.html and bump_static.sh to master state
    Revert accidental hash bumps to index.html. These files should not be
    modified in this PR — hash bumping happens at deploy time, not during
    development.

--- scripts/bump_static.sh
+++ scripts/bump_static.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Bump static asset cache-busting hashes in index.html.
-# Idempotent: replaces any existing hash value, not just the literal "csshash".
-
 csshash=`sha1sum src/static/style.css | awk '{print substr($0,1,8)}'`
 jshash=`cat src/static/main.js src/static/modules/*.js | sha1sum | awk '{print substr($0,1,8)}'`
 
-sed -i -E "s/style\\.css\\?hash=[a-z0-9]+/style.css?hash=$csshash/g" src/static/index.html
-sed -i -E "s/main\\.js\\?hash=[a-z0-9]+/main.js?hash=$jshash/g" src/static/index.html
+sed -i "s/csshash/$csshash/g" src/static/index.html
+sed -i "s/jshash/$jshash/g" src/static/index.html
